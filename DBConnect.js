8aafb62352a0b8a5be3bf46a0d4774bb3a3116a8
Gundapu <Suresh.Gundapu@Honeywell.com>
2025-02-21 18:39:35 +0530

    Add Environmental vraibles

--- DBConnect.js
+++ DBConnect.js
@@ -4,8 +4,9 @@ const connectDB = async () => {
   try {
     console.log("🔄 Trying to connect to MongoDB...");
     await mongoose.connect(
+      process.env.MONGO_URI,
       // "mongodb://127.0.0.1:27017/myLocalDB",//local DB
-      "mongodb+srv://suresh_admin:suresh_admin@mycluster.pkmr4.mongodb.net/myLocalDB?retryWrites=true&w=majority", //cloud DB
+      //"mongodb+srv://suresh_admin:suresh_admin@mycluster.pkmr4.mongodb.net/myLocalDB?retryWrites=true&w=majority", //cloud DB
       {
         serverSelectionTimeoutMS: 5000,
         connectTimeoutMS: 10000,
--- crudApp.js
+++ crudApp.js
@@ -3,7 +3,7 @@ const express = require("express");
 const connectDB = require("./DBConnect");
 const Users = require("./Models/Users");
 
-const PORT = 7000;
+const PORT = process.env.PORT || 7000; // Use Render's assigned port
 const app = express();
 
 app.use(express.json()); //middleware
